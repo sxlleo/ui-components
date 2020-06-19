6981d91b02c7180b66d479732f2323bbf9086f56
undefined <undefined>
2020-06-19 14:04:08 +0800

    Updates

--- src/packages/button/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <button class="my-button">Click</button>
-</template>
-
-<script>
-export default {
-  name: 'MyButton',
-}
-</script>
--- src/packages/link/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <a class="my-link">Link</a>
-</template>
-
-<script>
-export default {
-  name: 'MyLink',
-}
-</script>
